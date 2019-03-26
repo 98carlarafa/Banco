6be8a77a6853dea1ce4f692c27d6a80c5ffd2e99
ifnmg <ifnmg@192.168.163.20>
2019-03-26 20:38:51 -0300

    Aula 26-03

---
+++ src/TestaPessoa.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author ifnmg
+ */
+public class TestaPessoa {
+    
+    public static void main(String[] args) {
+        
+        Pessoa p1 = new Pessoa ();
+        p1.nome = "Carla Rafaela";
+        p1.idade = 20;
+        p1.cpf = "1234567890";
+        
+    }
+}
