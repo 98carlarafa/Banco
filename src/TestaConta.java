df374b2c806793e54de49641a13b5e065db8a3bf
ifnmg <ifnmg@192.168.163.20>
2019-03-26 20:23:24 -0300

    Aula 26-03

--- src/TestaConta.java
+++ src/TestaConta.java
@@ -12,7 +12,18 @@ public class TestaConta {
     
     public static void main(String[] args) {
         
-        Conta c1 = new Conta ()
+        Conta c1 = new Conta ();
+        c1.saldo = 9999;
+        c1.titular = "Lara Caroline";
+        c1.agencia = "1264";
+        c1.numero = "5432";
+        
+        Conta c2 = new Conta ();
+        c2.saldo = 500;
+        c2.titular = "Luciano Souza";
+        c2.agencia = "007";
+        c2.numero = "999";
+                    
         
     }
 }
